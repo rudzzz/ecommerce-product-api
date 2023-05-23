9b3f47afa284c6cb969916fc66c60623a5c26e14
rudzzz <rudsolza13@gmail.com>
2023-05-22 22:19:54 -0300

    Models folder

---
+++ Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace HPlusSportAPI.Models
+{
+	public class Product
+	{
+		public int Id { get; set; }
+		[Required]
+		public string Name { get; set; }
+		[Required]
+		public string Sku { get; set; }
+		[Required]
+		public string Description { get; set; } = string.Empty;
+		public decimal Price { get; set; }
+		public bool IsAvailable { get; set; }
+		[Required]
+		public int CategoryId { get; set; }
+		[JsonIgnore]
+		public virtual Category? Category { get; set; }
+	}
+}
---
+++ Models/ShopContext.cs
@@ -0,0 +1,27 @@
+﻿using HPlusSport.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HPlusSportAPI.Models
+{
+	public class ShopContext : DbContext
+	{
+		public ShopContext(DbContextOptions<ShopContext> options) : base(options)
+		{
+			
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Category>()
+				.HasMany(c => c.Products)
+				.WithOne(a => a.Category)
+				.HasForeignKey(a => a.CategoryId);
+
+			modelBuilder.Seed();
+		}
+
+		public DbSet<Product> Products { get; set; }
+		public DbSet<Category> Categories { get; set; }
+	}
+
+}
